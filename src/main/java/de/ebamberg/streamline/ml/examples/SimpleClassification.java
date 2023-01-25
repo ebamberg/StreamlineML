e10bb77cc2e7f23c20a4c54a4bac523348d03231
Erik Bamberg <erik.bamberg@web.de>
2023-01-25 15:15:45 +0000

    added softmax activation

--- src/main/java/de/ebamberg/streamline/ml/examples/SimpleClassification.java
+++ src/main/java/de/ebamberg/streamline/ml/examples/SimpleClassification.java
@@ -1,7 +1,9 @@
 package de.ebamberg.streamline.ml.examples;
 
 import ai.djl.ndarray.NDManager;
+import ai.djl.ndarray.types.Shape;
 import de.ebamberg.streamline.ml.activation.ReLUActivation;
+import de.ebamberg.streamline.ml.activation.SoftMaxActivation;
 import de.ebamberg.streamline.ml.layer.DenseLayer;
 
 public class SimpleClassification {
@@ -18,14 +20,19 @@ public class SimpleClassification {
 			
 			var layer1=new DenseLayer(manager, x.getShape());
 			var activation1=new ReLUActivation();
+			var layer2=new DenseLayer(manager, new Shape(1,5));
+			var activation2=new SoftMaxActivation();
 			
 			var output1=layer1.forward(x);
 			var output2=activation1.forward(output1);
+			var output3=layer2.forward(output2);
+			var output4=activation2.forward(output3);
 			
 			
 			System.out.printf("input data: %s\n", x.toString());
 			System.out.printf("after dense layer: %s\n", output1.toString());
 			System.out.printf("after activation: %s\n", output2.toString());
+			System.out.printf("output: %s\n", output4.toString());
 
 		}
 
